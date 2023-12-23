3bcd27018b1d5240877cfd523d98145a0ed8ab62
chenwei15317 <145735022+chenwei15317@users.noreply.github.com>
2023-12-23 21:34:22 +0800

    Add files via upload

---
+++ Ex13_03.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Ex13_03 {
+    public static void main(String[] args) {
+        int num = 12, den;
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            den = sc.nextInt();
+
+            if (den != 0) {
+                int ans = num / den;
+                System.out.println("答案: " + ans);
+            } else {
+                System.out.println("除數為0");
+            }
+        } 
+
+        catch (ArithmeticException e) {
+            System.out.println("捕捉到 ArithmeticException 例外: " + e.getMessage());
+        } 
+
+        finally {
+            sc.close();
+            System.out.println("end of main()!!");
+        }
+    }
+}
